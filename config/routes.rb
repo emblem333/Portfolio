ff11f152cbca4b85fd423387782bfbf39da33093
emblem333 <122144556+emblem333@users.noreply.github.com>
2023-04-10 07:43:20 +0000

    ﾛｸﾞｲﾝ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#devise_for :customers
+  # ユーザー用
+# URL /customers/sign_in ...
+devise_for :users, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
 end
