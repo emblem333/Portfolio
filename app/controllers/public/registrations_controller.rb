7712b325940c224b9ee93411b57c7ee9bb633338
emblem333 <122144556+emblem333@users.noreply.github.com>
2023-04-13 04:46:43 +0000

    ﾕｰｻﾞｰ編集機能追加

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
   def after_sign_up_path_for(resource)
@@ -9,6 +10,10 @@ class Public::RegistrationsController < Devise::RegistrationsController
 
   def after_update_path_for(resource)
     mypage_path
+  end
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
   # GET /resource/sign_up
   # def new
--- db/schema.rb
+++ db/schema.rb
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2023_04_11_053237) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
